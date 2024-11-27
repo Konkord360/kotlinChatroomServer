4761fe557dadc9cf5b43c12573e038236e23419a
Kondzitsu <Konkord720@gmail.com>
2024-11-27 18:30:46 +0100

    Added basic chat history persistance with sqlite and batch inserts

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,7 @@
 package org.exapmle
 
 import com.sun.security.ntlm.Client
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
@@ -13,23 +10,34 @@ import java.io.PrintWriter
 import java.net.ServerSocket
 import java.net.Socket
 import java.net.http.HttpRequest
+import java.sql.Connection
 import java.time.LocalDateTime
+import java.sql.SQLException
+import java.sql.DriverManager
+import java.time.format.DateTimeFormatter
+
 
 //Next time repair the get message history, add chatter names, add database
 data class Chatter(val name: String, val connection: Socket)
-data class Message(val chatter: Chatter, val message: String, val timestamp: LocalDateTime)
+data class Message(val chatterName: String, val message: String, val timestamp: LocalDateTime)
 
 val chatters = mutableListOf<Chatter>()
 val messages = arrayListOf<Message>()
+val newMessages = arrayListOf<Message>()
+const val url: String = "jdbc:sqlite:/home/kondzitsu/Projects/Kotlin/kotlinChatroomServer/test.db"
+val dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
 
 fun main() = runBlocking {
     val server = ServerSocket(8080)
-
     println("Starting server")
+
+    val connection = prepareDatabase()
+    loadChatHistory(connection)
+
     while (true) {
         println("Waiting for connection...")
 
-        val clientConnection = withContext(Dispatchers.IO) {server.accept()}
+        val clientConnection = withContext(Dispatchers.IO) { server.accept() }
         val inputReader = BufferedReader(InputStreamReader(clientConnection.inputStream))
         val output = BufferedWriter(OutputStreamWriter(clientConnection.outputStream))
 //        val output = PrintWriter(clientConnection.getOutputStream(), true)
@@ -48,13 +56,86 @@ fun main() = runBlocking {
         println("new connection from $customerName. Launching a coroutine")
         sendChatHistory(messages, chatter)
 
-        launch (Dispatchers.IO) {
+        launch(Dispatchers.IO) {
+            scheduleMessagePersistance(connection)
+        }
+
+        launch(Dispatchers.IO) {
             clientConnection.use {
                 clientConnection.handleClientConnection(chatter)
             }
         }
     }
 }
+
+fun prepareDatabase(): Connection {
+    val connection = DriverManager.getConnection(url)!!
+
+    if (connection.metaData.getTables(null, null, "MESSAGES", arrayOf("MESSAGES")).next()) {
+        println("messages table missing")
+        connection.createStatement().use { statement ->
+            statement.execute(
+                "CREATE TABLE MESSAGES(" +
+                        "id integer PRIMARY KEY," +
+                        "username TEXT," +
+                        "message TEXT, " +
+                        "timestamp TEXT" +
+                        ");"
+            )
+        }
+        println("MESSAGES table created")
+    }
+    println("Connection to SQLite has been established.")
+
+    return connection;
+}
+
+fun loadChatHistory(connection: Connection) {
+    println("Loading chat history from database")
+    connection.createStatement().use { statement ->
+        statement.executeQuery("SELECT username, message, timestamp FROM MESSAGES").use { resultSet ->
+            while (resultSet.next()) {
+                messages.add(
+                    Message(
+                        resultSet.getString(1),
+                        resultSet.getString(2),
+                        LocalDateTime.parse(resultSet.getString(3), dateTimeFormatter)
+                    )
+                )
+            }
+        }
+    }
+    println("Chat history loaded")
+}
+
+fun persistNewMessages(connection: Connection) {
+    val localMessages = copyAndClearMessages()
+
+    val preparedStatement =
+        connection.prepareStatement("INSERT INTO MESSAGES(username, message, timestamp) values (?, ?, ?)")
+
+    for (message in localMessages) {
+        preparedStatement.setString(1, message.chatterName)
+        preparedStatement.setString(2, message.message)
+        preparedStatement.setString(3, message.timestamp.format(dateTimeFormatter))
+        preparedStatement.addBatch()
+    }
+
+    val resultArray = preparedStatement.executeBatch()
+    if (resultArray.sum() == resultArray.size) {
+        println("Added new messages to the history. Cleared new messages list")
+    } else {
+        println("Database update failed")
+    }
+    println("Exiting persisting new messages")
+}
+
+@Synchronized fun copyAndClearMessages() :MutableList<Message> {
+    val localMessages: MutableList<Message> = newMessages.toMutableList()
+    newMessages.clear()
+    return localMessages
+}
+
 
 fun BufferedWriter.println(message: String) {
     this.write(message)
@@ -67,7 +148,7 @@ fun Socket.handleClientConnection(chatter: Chatter) {
     val output = PrintWriter(this.getOutputStream(), true)
     println("message history: $messages")
 
-    while(true) {
+    while (true) {
         val response = inputReader.readLine()
         if (response == null) {
             println("client disconnected")
@@ -75,11 +156,13 @@ fun Socket.handleClientConnection(chatter: Chatter) {
         }
 
         synchronized(messages) {
-            messages.add(Message(chatter, response, LocalDateTime.now()))
+            synchronized(newMessages) {
+                messages.add(Message(chatter.name, response, LocalDateTime.now()))
+                newMessages.add(Message(chatter.name, response, LocalDateTime.now()))
+            }
         }
-
         println(response)
-        output.println("$response back")
+        output.println("$messages")
     }
 }
 
@@ -93,4 +176,12 @@ fun sendChatHistory(history: List<Message>, client: Chatter) {
     println("Sending chat history to a client")
     val output = PrintWriter(client.connection.getOutputStream(), true)
     output.println(history)
+}
+
+suspend fun scheduleMessagePersistance(connection: Connection) {
+    while (true) {
+        delay(5000)
+        println("persisting new messages")
+        persistNewMessages(connection)
+    }
 }
